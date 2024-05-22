377e00b265c29280e35e8ab2e1d2c7480f2d7d96
malumzz <79271163+malumzz@users.noreply.github.com>
2024-05-22 15:48:26 +0200

    changes to social page

--- src/Components/Social/LeftMain.jsx
+++ src/Components/Social/LeftMain.jsx
@@ -1,14 +1,20 @@
-import react from 'react';
-import LeftMain1 from './LeftMain1';
-import LeftMain2 from './LeftMain2';
+import React from 'react';
 
 const LeftMain = () => {
-    return (
-        <div>
-            <LeftMain1 />
-            <LeftMain2 />
+return (
+<div className="left-main">
+    {/* LeftMain1 content */}
+    <div className="left-main-1">
+        <img className="banner" src="/public/assets/images/photos/post.jpg" alt="Banner" height="59px" width="500px" />
+        <img className="my-picture" src="" alt="Profile Picture" />
+        <div className="about">
+            <h5>USERNAME</h5>
+            <p>IT Student<br />Optimistic</p>
         </div>
-    );
+    </div>
+
+</div>
+);
 };
 
 export default LeftMain;
